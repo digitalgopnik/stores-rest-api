49ca512234c49f6d0dad9697a025e7795616e8a2
Christian Ratz <christian.ratz@b-ite.de>
2021-12-15 19:48:22 +0100

    fix postgresql-dsn

--- app.py
+++ app.py
@@ -13,7 +13,11 @@ app = Flask(__name__)
 
 app.config['DEBUG'] = True
 
-app.config['SQLALCHEMY_DATABASE_URI'] = os.environ.get('DATABASE_URL', 'sqlite:///data.db')
+database_uri = os.environ.get('DATABASE_URL')
+if len(database_uri) > 0:
+    app.config['SQLALCHEMY_DATABASE_URI'] = database_uri.replace('postgres', 'postgresql')
+else:
+    app.config['SQLALCHEMY_DATABASE_URI'] = 'sqlite:///data.db'
 app.config['SQLALCHEMY_TRACK_MODIFICATIONS'] = False
 app.secret_key = 'jose'
 api = Api(app)
